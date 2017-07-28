5bbac808398d14131b8f1c600df503b28724dcc3
Darryl Anaud <darryl@plus65.com.sg>
2017-07-28 19:50:18 +0800

    Change LTC to P65 in src folder

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("P65");
+    case mBTC: return QString("mP65");
+    case uBTC: return QString::fromUtf8("μP65");
     default: return QString("???");
     }
 }
